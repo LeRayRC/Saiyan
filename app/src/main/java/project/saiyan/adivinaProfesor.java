964d0d4bb970f2120ef9e7892e67cb228dce57c3
Carlos <cargarr9@teleco.upv.es>
2018-06-17 14:04:24 +0200

    Intent finalizado, arreglando las putas imagenes

--- app/src/main/java/project/saiyan/adivinaProfesor.java
+++ app/src/main/java/project/saiyan/adivinaProfesor.java
@@ -9,5 +9,6 @@ public class adivinaProfesor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adivina_profesor);
+
     }
 }
