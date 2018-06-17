0f2ac75547fbdcbd70532b23106494024884f572
Carlos <cargarr9@teleco.upv.es>
2018-06-17 03:13:11 +0200

    Inicio del intent

--- app/src/main/java/project/saiyan/MainActivity.java
+++ app/src/main/java/project/saiyan/MainActivity.java
@@ -2,6 +2,7 @@ package project.saiyan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /** Llamado cuando el jugador pulse el botón de Jugar */
+    public void iniciarJuego(View view){
+
     }
 }
