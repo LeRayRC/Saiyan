50dd0fce899ff0ae9077cf995c4764e7e9099090
Carlos <cargarr9@teleco.upv.es>
2018-06-19 01:57:36 +0200

    seleccion aleatoria de profesor y representacion de las cuatro imagenes asociadas.

--- app/src/main/java/project/saiyan/adivinaProfesor.java
+++ app/src/main/java/project/saiyan/adivinaProfesor.java
@@ -9,7 +9,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.lang.reflect.Field;
 
 public class adivinaProfesor extends AppCompatActivity {
 
@@ -18,21 +17,50 @@ public class adivinaProfesor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adivina_profesor);
 
-        final EditText introducir_respuesta = findViewById(R.id.respuesta);
-        int res_id_nombre = getResources().getIdentifier("profesor_001","array",getPackageName());
+        /* Generamos un int random entre 1 y 2. De momento solo hay dos profesroes */
+        final int selector = 1 + (int)(Math.random()*2);
+
+        /* Accedemos al XML. */
+        final String profesor_nombres = "profesor_00" + selector;
+        final String profesor_imagenes = "profesor_00" + selector + "_imagenes";
+        final int res_id_nombre = getResources().getIdentifier(profesor_nombres,"array",getPackageName());
         final String[] nombres_profesor = getResources().getStringArray(res_id_nombre);
-        final ImageView imagen_1 = findViewById(R.id.imagen_profesor);
-        /**
-        final ImageView imagen_2 = findViewById(R.id.imagen_profesor);
-        final ImageView imagen_3 = findViewById(R.id.imagen_profesor);
-        final ImageView imagen_4 = findViewById(R.id.imagen_profesor);
-         */
+        final int res_id_imagenes = getResources().getIdentifier(profesor_imagenes,"array",getPackageName());
+        final String[] imagenes_profesor = getResources().getStringArray(res_id_imagenes);
+
+        /* Aqui introduciremos la respuesta */
+        final EditText introducir_respuesta = findViewById(R.id.respuesta);
+
+        /* Las cuatro imagenes del profesor. */
+        final ImageView imagen_1 = findViewById(R.id.imagen_1);
+        final ImageView imagen_2 = findViewById(R.id.imagen_2);
+        final ImageView imagen_3 = findViewById(R.id.imagen_3);
+        final ImageView imagen_4 = findViewById(R.id.imagen_4);
+
         final Toast respuesta_incorrecta = Toast.makeText(getApplicationContext(), "Respuesta incorrecta", Toast.LENGTH_SHORT);
 
-        for(int i = 3;i<7;i++) {
-            int res_id = getResources().getIdentifier("telematica", "drawable", getPackageName());
-            imagen_1.setImageResource(res_id);
+        for(int i = 0;i<4;i++) {
+            final int res_id = getResources().getIdentifier(imagenes_profesor[i], "drawable", getPackageName());
+            switch (i) {
+                case 0: {
+                    imagen_1.setImageResource(res_id);
+                    break;
+                }
+                case 1: {
+                    imagen_2.setImageResource(res_id);
+                    break;
+                }
+                case 2: {
+                    imagen_3.setImageResource(res_id);
+                    break;
+                }
+                case 3: {
+                    imagen_4.setImageResource(res_id);
+                    break;
+                }
+            }
         }
+
 
         Button responder = findViewById(R.id.boton_respuesta);
         responder.setOnClickListener(new View.OnClickListener() {
@@ -49,14 +77,5 @@ public class adivinaProfesor extends AppCompatActivity {
                 }
             }
         });
-    }
-    public static int getResStringId(String variableName) {
-        try {
-            Field idField = R.string.class.getDeclaredField(variableName);
-            return idField.getInt(idField);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return -1;
-        }
     }
 }
